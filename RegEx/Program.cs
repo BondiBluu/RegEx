bb1e547d111d30d6c4fda4ffdf3aecdf3e718a30
bamideleogundeji <bamideleogundeji949@gmail.com>
2025-02-13 13:15:28 -0500

    Add project files.

---
+++ RegEx/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ways to initialize
+            Regex regex = new Regex(@"\d{3}-\d{2}-\d{4}");
+            string pattern = @"\d{3}-\d{2}-\d{4}";
+            Regex regex1 = new Regex(pattern);
+
+            string text = "My SSN is 123-45-6789";
+
+            //matching the regex pattern with string text
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine("Matches: " + matches.Count);
+
+            string text2 = "My SSNs are 123-45-6789, 222-33-4567, 888-43-5675";
+
+            Console.WriteLine("Matches: " + regex.Matches(text2).Count);
+        }
+    }
+}
